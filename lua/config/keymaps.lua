af8318c9c6e041f204715885ac38ada240b1f17c
Kevin Coyle <kevin.coyle@drutek.co.uk>
2024-06-14 12:14:38 +0100

    Still adding things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = LazyVim.safe_keymap_set
+map("n", "<C-9>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-0>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
